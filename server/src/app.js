acf92ba26e68a2f1bfde5b63d298854ca097e39f
Yehonatan <yehonatanrefael@gmail.com>
2024-08-19 16:14:33 +0300

    mini server

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket connected: " + socket.id);
+  socket.on("ping", (msg) => {
+    console.log(msg);
+    io.emit("pimg", msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
